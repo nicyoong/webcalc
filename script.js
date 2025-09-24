05d29c0a172b667736daa1bc5a52620be70eb60f
Nic <73386479+nicyoong@users.noreply.github.com>
2025-09-25 03:42:18 +0800

    create class calculator

---
+++ script.js
@@ -0,0 +1,6 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
